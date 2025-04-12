e79117f7cb50542c9551c724e0dd2e7e76cae885
Roberto Velazquez <rochugui10@gmail.com>
2025-04-12 11:06:41 -0400

    Fix typo in username label and placeholder in index.php; add custom styles for header in header.php

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
      <?php echo display_msg($msg); ?>
       <form method="post" action="auth.php" class="clearfix">
         <div class="form-group">
-              <label for="username" class="control-label">Usario</label>
-              <input type="name" class="form-control" name="username" placeholder="Usario">
+              <label for="username" class="control-label">Usuario</label>
+              <input type="name" class="form-control" name="username" placeholder="Usuario">
         </div>
         <div class="form-group">
             <label for="Password" class="control-label">Contraseña</label>
--- layouts/header.php
+++ layouts/header.php
@@ -13,6 +13,16 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" />
     <link rel="stylesheet" href="libs/css/main.css" />
+    <style>
+      #header {
+        background-color: #181818;
+        border-bottom: 3px solid #ff5a5f;
+      }
+      #header .logo,
+      #header .header-content {
+        color: #ffffff;
+      }
+    </style>
   </head>
   <body>
   <?php  if ($session->isUserLoggedIn(true)): ?>
